f27d97c26cec4dadeb3e66b6d6afc56aba553512
k28 <k28.mrn@gmail.com>
2024-12-05 19:30:12 +0900

    MIDIサンプル追加

---
+++ 10_midi-controller/src/renderer/scripts/gui/settings/midi-gui.ts
@@ -0,0 +1,123 @@
+import { BindingApi, FolderApi, } from "@tweakpane/core";
+import { GuiBase } from "./gui-base";
+import { MidiEventProps, MidiProps } from "@common/interfaces";
+/**
+ * シリアル制御用GUIクラス
+ */
+export class MidiGui extends GuiBase {
+	static MidiMessage = 'MidiMessage';
+	deviceName: string = '';
+	device: WebMidi.MIDIInput = undefined;
+	debug: string = '';
+	#deviceList: { [key: string]: WebMidi.MIDIInput; } = {};
+	#deviceKeyList: { [key: string]: string; } = {};
+	#deviceBinding: BindingApi;
+	#debugBinding: BindingApi;
+
+	constructor(folder: FolderApi, useConfig: boolean, data: MidiProps,) {
+		super(folder);
+		this.folder.hidden = !useConfig;
+		this.deviceName = data.deviceName;
+		this.setup();
+	}
+
+	/**
+	 * 設定情報
+	 */
+	get config(): MidiProps {
+		const deviceName = this.folder.hidden ? '' : this.deviceName;
+		return { deviceName };
+	}
+
+	/**
+	 * setup
+	 */
+	setup = async () => {
+		await this.#getMidiInputs();
+		this.#setDeviceList();
+		this.#debugBinding = this.folder.addBinding(this, "debug", { label: 'Debug', readonly: true, multiline: true, rows: 6, });
+	};
+
+	/**
+	 * デバイスリスト設定
+	 */
+	#setDeviceList = () => {
+		if (this.#deviceBinding) this.#deviceBinding.dispose();
+		// GUI設定
+		this.#deviceBinding = this.folder.addBinding(this, "deviceName", { label: 'Device', options: this.#deviceKeyList, }).on('change', (_) => {
+			// 既に選択されてるデバイス一度閉じる
+			if (this.device) {
+				this.device.onmidimessage = null;
+				this.device.close();
+			}
+			// 新しいデバイスを設定
+			this.device = this.#deviceList[this.deviceName];
+			this.onChangeConfig();
+			this.#startListener();
+		});
+
+		// 保存してた情報がありリストの中に存在していれば設定
+		if (this.deviceName in this.#deviceList) {
+			this.device = this.#deviceList[this.deviceName];
+		}
+		this.#startListener();
+	};
+
+	/**
+	 * MIDIデバイスのリストを取得する
+	 */
+	#getMidiInputs = async () => {
+		this.#deviceList = {};
+		try {
+			const midiAccess = await window.navigator.requestMIDIAccess();
+			const midi = midiAccess;
+			const inputs = midi.inputs;
+			inputs.forEach((input) => {
+				this.#deviceList[input.name] = input;
+				this.#deviceKeyList[input.name] = input.name;
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	/**
+	 * MIDIイベントリスナーを開始する
+	 */
+	#startListener = () => {
+		if (this.device === undefined) return;
+		this.device.onmidimessage = this.#onMidiMessage;
+	};
+
+	/**
+	 * データ送信
+	 */
+	#onMidiMessage = (message: WebMidi.MIDIMessageEvent) => {
+		// console.log(message, message.data);
+		const data = message.data;
+		const cmd = data[0] >> 4;
+		const channel = data[0] & 0xf;
+		const type = data[0] & 0xf0;
+		const note = data[1];
+		const velocity = data[2];
+
+		this.debug = `cmd: ${cmd}\n`;
+		this.debug += `channel: ${channel}\n`; //MIDIチャンネル
+		this.debug += `type: ${type}\n`; //MIDIメッセージのタイプ
+		this.debug += `note: ${note}\n`; //ノート番号（キー）
+		this.debug += `velocity: ${velocity}\n`; //鍵盤を押す強さ (フェーダー・ノブの場合は操作値 / ボタンの場合は 0 or 127)
+
+		// イベント発火
+		window.dispatchEvent(new CustomEvent<MidiEventProps>(MidiGui.MidiMessage, {
+			detail: {
+				message: message,
+				cmd: cmd,
+				channel: channel,
+				type: type,
+				note: note,
+				velocity: velocity,
+			}
+		}));
+	};
+}
+
---
+++ 10_midi-controller/src/renderer/scripts/sketch.ts
@@ -0,0 +1,44 @@
+import p5 from "p5";
+import { App } from "./gui/app-gui";
+import { MidiEventProps } from "@common/interfaces";
+
+/**
+ * MIDIコントローラ制御サンプル
+ * MIDIデータを受け取り演出する
+ */
+export const sketch = (p: p5): void => {
+	// DMXデータ
+	let data = new Uint8Array(512);
+
+	/**
+	 * Setup
+	 */
+	p.setup = (): void => {
+		p.createCanvas(p.windowWidth, p.windowHeight);
+		p.background(255);
+
+		// MIDIキーボードイベント登録
+		window.addEventListener("MidiMessage", onGetMidiMessage);
+	};
+
+	/**
+	 * Draw
+	 */
+	p.draw = (): void => {
+
+	};
+
+	/**
+	 * MIDIキーボードが押されてイベントが発生した時の処理
+	 */
+	const onGetMidiMessage = (event: CustomEvent<MidiEventProps>): void => {
+		console.log(event.detail);
+	};
+
+	/**
+	 * Window Resized
+	 */
+	p.windowResized = (): void => {
+		p.resizeCanvas(p.windowWidth, p.windowHeight);
+	};
+};
