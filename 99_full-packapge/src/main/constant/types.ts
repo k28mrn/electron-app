a4acf3de3e31fc12c7ca27618fcd5e442e36067f
k28 <k28.mrn@gmail.com>
2025-12-11 20:53:12 +0900

    フルパッケージ削除

--- 99_full-packapge/src/main/constant/types.ts
+++
@@ -1,9 +0,0 @@
-import { AppStoreProps } from '@common/interfaces';
-/**
- * アプリケーション設定
- */
-export interface MainAppStoreProps extends AppStoreProps {
-	browser?: Electron.BrowserWindowConstructorOptions;
-	guiDisplay?: boolean;
-}
-
